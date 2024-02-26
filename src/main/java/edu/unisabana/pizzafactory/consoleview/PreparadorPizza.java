c853813c3d2e9ab46f43f416c86c932119741cd0
Cesar Vega <cesar.a.vega.f@gmail.com>
2024-02-25 21:54:53 -0500

    Taller 1 - GoF-Patterns-Creational-PizzaFactory

---
+++ src/main/java/edu/unisabana/pizzafactory/consoleview/PreparadorPizza.java
@@ -0,0 +1,60 @@
+
+package edu.unisabana.pizzafactory.consoleview;
+
+import edu.unisabana.pizzafactory.model.AmasadorPizzaDelgada;
+import edu.unisabana.pizzafactory.model.ExcepcionParametrosInvalidos;
+import edu.unisabana.pizzafactory.model.HorneadorPizzaDelgada;
+import edu.unisabana.pizzafactory.model.Ingrediente;
+import edu.unisabana.pizzafactory.model.MoldeadorPizzaDelgada;
+import edu.unisabana.pizzafactory.model.Tamano;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author cesarvefe
+ */
+public class PreparadorPizza {
+
+    public static void main(String args[]){
+        try {
+            Ingrediente[] ingredientes=new Ingrediente[]{new Ingrediente("queso",1),new Ingrediente("jamon",2)};            
+            PreparadorPizza pp=new PreparadorPizza();            
+            pp.prepararPizza(ingredientes, Tamano.MEDIANO);
+        } catch (ExcepcionParametrosInvalidos ex) {
+            Logger.getLogger(PreparadorPizza.class.getName()).log(Level.SEVERE, "Problema en la preparacion de la Pizza", ex);
+        }
+                
+    }
+    
+    
+    public void prepararPizza(Ingrediente[] ingredientes, Tamano tam)
+            throws ExcepcionParametrosInvalidos {
+        AmasadorPizzaDelgada am = new AmasadorPizzaDelgada();
+        HorneadorPizzaDelgada hpd = new HorneadorPizzaDelgada();
+        MoldeadorPizzaDelgada mp = new MoldeadorPizzaDelgada();
+        am.amasar();
+        if (tam == Tamano.PEQUENO) {
+            mp.moldearPizzaPequena();
+        } else if (tam == Tamano.MEDIANO) {
+            mp.molderarPizzaMediana();
+        } else {
+            throw new ExcepcionParametrosInvalidos("Tamano de piza invalido:"+tam);
+        }
+	aplicarIngredientes(ingredientes);
+        hpd.hornear();
+    }
+
+    private void aplicarIngredientes(Ingrediente[] ingredientes) {
+        Logger.getLogger(PreparadorPizza.class.getName())
+                .log(Level.INFO, "APLICANDO INGREDIENTES!:{0}", Arrays.toString(ingredientes));
+        
+        //CODIGO DE LLAMADO AL MICROCONTROLADOR
+        
+        
+        
+    }
+
+
+}
